0774cf654e35ef2e38ca9718fe4605ada42409e7
Rama Alamaireh <139100580+Rama-Alamaireh@users.noreply.github.com>
2025-05-25 18:26:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -283,7 +283,7 @@ if uploaded_file is not None:
             fig_circle.update_layout(
                 paper_bgcolor='rgba(0,0,0,0)',
                 font={'color': "white", 'family': "Arial"},
-                margin=dict(t=50, b=0, l=0, r=0)
+                margin=dict(t=50, b=0, l=0, r=0),
                 height=300,
             )
 
